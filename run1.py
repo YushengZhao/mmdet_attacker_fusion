1b3f3b7184a8d4b9afee52788d8967ae60a1e46a
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-19 15:02:35 +0800

    optimized

--- run1.py
+++ run1.py
@@ -29,10 +29,11 @@ rcnn_model = init_detector(config, checkpoint, device='cuda:1')  # 构建 faster
 clean_path = 'select1000_new/'  # 干净图片目录
 dirty_path = 'select1000_new_p/'  # 对抗图片存放位置
 imgs_list = os.listdir(clean_path)
+imgs_list.sort()
 mesh = np.zeros((500, 500, 3))
 mesh[::5, :, :] = 1
 mesh[:, ::5, :] = 1
-for i in range(len(imgs_list)):
+for i in range(len(imgs_list[:500])):
     image_name = os.path.basename(imgs_list[i]).split('.')[0]  # 测试图片名称
     print('It is attacking on the {}-th image, the image name is {}'.format(i, image_name))
     image_path = os.path.join(clean_path, imgs_list[i])
