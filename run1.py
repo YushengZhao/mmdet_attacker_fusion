a96a7557783f61ec8c3d57bc10b220729535d03e
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-19 15:08:08 +0800

    parallelized

--- run1.py
+++ run1.py
@@ -33,7 +33,7 @@ imgs_list.sort()
 mesh = np.zeros((500, 500, 3))
 mesh[::5, :, :] = 1
 mesh[:, ::5, :] = 1
-for i in range(len(imgs_list[500:])):
+for i in range(500, len(imgs_list)):
     image_name = os.path.basename(imgs_list[i]).split('.')[0]  # 测试图片名称
     print('It is attacking on the {}-th image, the image name is {}'.format(i, image_name))
     image_path = os.path.join(clean_path, imgs_list[i])
