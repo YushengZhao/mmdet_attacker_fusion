a70bfed0334a755ee5c7e01af489a0cbab3f0b26
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-19 23:15:20 +0800

    7*7

--- run1.py
+++ run1.py
@@ -31,9 +31,9 @@ dirty_path = 'select1000_new_p/'  # 对抗图片存放位置
 imgs_list = os.listdir(clean_path)
 imgs_list.sort()
 mesh = np.zeros((500, 500, 3))
-mesh[::5, :, :] = 1
-mesh[:, ::5, :] = 1
-for i in range(500, len(imgs_list)):
+mesh[::7, :, :] = 1
+mesh[:, ::7, :] = 1
+for i in range(len(imgs_list)):
     image_name = os.path.basename(imgs_list[i]).split('.')[0]  # 测试图片名称
     print('It is attacking on the {}-th image, the image name is {}'.format(i, image_name))
     image_path = os.path.join(clean_path, imgs_list[i])
