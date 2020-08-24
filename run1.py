a1a5c2684175867b7d6848feedab03a620335448
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-24 18:56:42 +0800

    dynamic mesh

--- run1.py
+++ run1.py
@@ -30,16 +30,29 @@ clean_path = 'select1000_new/'  # 干净图片目录
 dirty_path = 'select1000_new_p2/'  # 对抗图片存放位置
 imgs_list = os.listdir(clean_path)
 imgs_list.sort()
-mesh = np.zeros((500, 500, 3))
-mesh[::3, :, :] = 1
-mesh[:, ::6, :] = 1
+
+
+def get_mesh(mask, threshold=4950):
+    cnt = np.sum(mask) / 3
+    stride = 1
+    f = lambda x: (2 * x - 1) / (x * x)
+    while cnt * f(stride) >= threshold:
+        stride += 1
+    mesh = np.zeros((500, 500, 3))
+    mesh[::stride, :, :] = 1
+    mesh[:, ::stride, :] = 1
+    return mesh
+
+
 # imgs_list = imgs_list[500:]
 for i in range(len(imgs_list)):
     image_name = os.path.basename(imgs_list[i]).split('.')[0]  # 测试图片名称
     print('It is attacking on the {}-th image, the image name is {}'.format(i, image_name))
     image_path = os.path.join(clean_path, imgs_list[i])
     img = cv2.imread(image_path)
-    mask = np.load('Mask/1471/Mask/{}.npy'.format(image_name)) * mesh
+    mask = np.load('Mask/1471/Mask/{}.npy'.format(image_name))
+    mesh = get_mesh(mask)
+    mask = mask * mesh
     # finalimg, noise = str_attack(darknet_model, img, conf_thresh=0.35, max_iter=120, epsilon=10, mask=mask)
     finalimg, noise = ada_attack(darknet_model, rcnn_model, img, conf_thresh=0.3, max_iter=100, epsilon=6, mask=mask)
     # finalimg, noise = gen_attack(darknet_model, img, conf_thresh=0.35, max_iter=100, epsilon=2, mask=mask)
