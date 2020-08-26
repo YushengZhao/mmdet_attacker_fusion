e83014edf89e74eb1b16056054573172b65e6166
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-26 21:49:42 +0800

    2500

--- run1.py
+++ run1.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.append('./mmdetection/')
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 
 from mmdet.apis import init_detector
 import cv2
@@ -23,16 +23,16 @@ darknet_model = darknet_model.eval().cuda()
 # faster rcnn
 config = './mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 checkpoint = './models/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-rcnn_model = init_detector(config, checkpoint, device='cuda:0')  # 构建 faster rcnn
+rcnn_model = init_detector(config, checkpoint, device='cuda:1')  # 构建 faster rcnn
 
 # 循环攻击目录中的每张图片
 clean_path = 'select1000_new/'  # 干净图片目录
-dirty_path = 'select1000_new_p/'  # 对抗图片存放位置
+dirty_path = 'select1000_new_p2/'  # 对抗图片存放位置
 imgs_list = os.listdir(clean_path)
 imgs_list.sort()
 
 
-def get_mesh(mask, threshold=2500):
+def get_mesh(mask, threshold=2000):
     cnt = np.sum(mask) / 3
     stride = 1
     f = lambda x: (2 * x - 1) / (x * x)
