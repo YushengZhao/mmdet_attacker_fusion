9aeddce419044ad361b98543899ae87f15a62ab5
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-30 23:37:36 +0800

    iteration = 250

--- run1.py
+++ run1.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.append('./mmdetection/')
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 
 from mmdet.apis import init_detector
 import cv2
@@ -23,11 +23,11 @@ darknet_model = darknet_model.eval().cuda()
 # faster rcnn
 config = './mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 checkpoint = './models/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-rcnn_model = init_detector(config, checkpoint, device='cuda:0')  # 构建 faster rcnn
+rcnn_model = init_detector(config, checkpoint, device='cuda:1')  # 构建 faster rcnn
 
 # 循环攻击目录中的每张图片
 clean_path = 'select1000_new/'  # 干净图片目录
-dirty_path = 'select1000_new_p/'  # 对抗图片存放位置
+dirty_path = 'select1000_new_p2/'  # 对抗图片存放位置
 imgs_list = os.listdir(clean_path)
 imgs_list.sort()
 
@@ -54,7 +54,7 @@ for i in range(len(imgs_list)):
     # mesh = get_mesh(mask)
     # mask = mask * mesh
     # finalimg, noise = str_attack(darknet_model, img, conf_thresh=0.35, max_iter=120, epsilon=10, mask=mask)
-    finalimg, noise = ada_attack(darknet_model, rcnn_model, img, conf_thresh=0.35, max_iter=200, epsilon=6, mask=mask)
+    finalimg, noise = ada_attack(darknet_model, rcnn_model, img, conf_thresh=0.35, max_iter=250, epsilon=6, mask=mask)
     # finalimg, noise = gen_attack(darknet_model, img, conf_thresh=0.35, max_iter=100, epsilon=2, mask=mask)
     image_pert_path = os.path.join(dirty_path, imgs_list[i])
     finalimg.save(image_pert_path)
