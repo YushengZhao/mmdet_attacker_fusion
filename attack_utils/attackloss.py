c9b03e337e051cddbaaf07720b3d44f36a148ef5
YushengZhao <zhaoyusheng@buaa.edu.cn>
2020-08-30 23:34:17 +0800

    use conf_thresh=0.35

--- attack_utils/attackloss.py
+++ attack_utils/attackloss.py
@@ -206,11 +206,11 @@ def ada_attack(yolo_model, rcnn_model, img, conf_thresh, max_iter, epsilon, mask
         NP_P_rcnn = normalized_grad_rcnn * epsilon
         noise_rcnn += NP_P_rcnn.squeeze().transpose(1, 2, 0)
         NOISE = cv2.resize(noise_yolo, (500, 500)) * mask + cv2.resize(noise_rcnn, (500, 500)) * mask
-        ind_one = np.logical_and(NOISE >= 0, NOISE <= 1.1)
-        ind_minus_one = np.logical_and(NOISE >= -1.1, NOISE < 0)
-        NOISE[ind_one] = 1.1
-        NOISE[ind_minus_one] = -1.1
-        NOISE = NOISE * mask
+        # ind_one = np.logical_and(NOISE >= 0, NOISE <= 1.1)
+        # ind_minus_one = np.logical_and(NOISE >= -1.1, NOISE < 0)
+        # NOISE[ind_one] = 1.1
+        # NOISE[ind_minus_one] = -1.1
+        # NOISE = NOISE * mask
         if iter_n == 0:
             NOISE += np.ones_like(NOISE)
         temp_img = np.clip((img - NOISE[:, :, ::-1]), 0, 255)  # 改为int32
--- run1.py
+++ run1.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.append('./mmdetection/')
-os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 from mmdet.apis import init_detector
 import cv2
@@ -23,11 +23,11 @@ darknet_model = darknet_model.eval().cuda()
 # faster rcnn
 config = './mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 checkpoint = './models/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-rcnn_model = init_detector(config, checkpoint, device='cuda:1')  # 构建 faster rcnn
+rcnn_model = init_detector(config, checkpoint, device='cuda:0')  # 构建 faster rcnn
 
 # 循环攻击目录中的每张图片
 clean_path = 'select1000_new/'  # 干净图片目录
-dirty_path = 'select1000_new_p2/'  # 对抗图片存放位置
+dirty_path = 'select1000_new_p/'  # 对抗图片存放位置
 imgs_list = os.listdir(clean_path)
 imgs_list.sort()
 
@@ -50,11 +50,11 @@ for i in range(len(imgs_list)):
     print('It is attacking on the {}-th image, the image name is {}'.format(i, image_name))
     image_path = os.path.join(clean_path, imgs_list[i])
     img = cv2.imread(image_path)
-    mask = np.load('Mask/InitialMask/{}.npy'.format(image_name))
-    mesh = get_mesh(mask)
-    mask = mask * mesh
+    mask = np.load('Mask/mask2900/{}.npy'.format(image_name))
+    # mesh = get_mesh(mask)
+    # mask = mask * mesh
     # finalimg, noise = str_attack(darknet_model, img, conf_thresh=0.35, max_iter=120, epsilon=10, mask=mask)
-    finalimg, noise = ada_attack(darknet_model, rcnn_model, img, conf_thresh=0.3, max_iter=150, epsilon=6, mask=mask)
+    finalimg, noise = ada_attack(darknet_model, rcnn_model, img, conf_thresh=0.35, max_iter=200, epsilon=6, mask=mask)
     # finalimg, noise = gen_attack(darknet_model, img, conf_thresh=0.35, max_iter=100, epsilon=2, mask=mask)
     image_pert_path = os.path.join(dirty_path, imgs_list[i])
     finalimg.save(image_pert_path)
